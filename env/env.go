f82e2721465c0e9b78700523c2953cb2c964fadd
Ted KIM <ted.kim@lfin.kr>
2023-03-06 19:17:38 +0900

    GetEnv 리턴으로 key 존재여부 추가

--- env/env.go
+++ env/env.go
@@ -15,46 +15,54 @@ func SetEnv(key, value string) error {
 // GetEnv 환경변수 가져오기
 // key: 환경변수명
 // def: 기본값
-// return: 환경변수값, 키가 없으면 def 값
-func GetEnv(key, def string) string {
+// return:
+//  string: 환경변수값, 키가 없으면 def 값
+//  bool: 키가 있으면 true, 없으면 false
+func GetEnv(key, def string) (string, bool) {
 	if value, ok := os.LookupEnv(key); ok {
-		return value
+		return value, ok
 	}
-	return def
+	return def, false
 }
 
 // GetEnvInt 환경변수 가져오기
 // key: 환경변수명
 // def: 기본값
-// return: 환경변수값, 키가 없으면 def 값
-func GetEnvInt(key string, def int) int {
+// return:
+//	int: 환경변수값, 키가 없으면 def 값
+//	bool: 키가 있으면 true, 없으면 false
+func GetEnvInt(key string, def int) (int, bool) {
 	if value, ok := os.LookupEnv(key); ok {
 		iv, _ := strconv.Atoi(value)
-		return iv
+		return iv, ok
 	}
-	return def
+	return def, false
 }
 
 // GetEnvInt64 환경변수 가져오기
 // key: 환경변수명
 // def: 기본값
-// return: 환경변수값, 키가 없으면 def 값
-func GetEnvInt64(key string, def int64) int64 {
+// return:
+//  int64: 환경변수값, 키가 없으면 def 값
+//  bool: 키가 있으면 true, 없으면 false
+func GetEnvInt64(key string, def int64) (int64, bool) {
 	if value, ok := os.LookupEnv(key); ok {
 		iv, _ := strconv.ParseInt(value, 10, 64)
-		return iv
+		return iv, ok
 	}
-	return def
+	return def, false
 }
 
 // GetEnvBool 환경변수 가져오기
 // key: 환경변수명
 // def: 기본값
-// return: 환경변수값, 키가 없으면 def 값, 1,True,true-->true, 그외 false
-func GetEnvBool(key string, def bool) bool {
+// return:
+//  bool: 환경변수값, 키가 없으면 def 값, 1,True,true-->true, 그외 false
+//  bool: 키가 있으면 true, 없으면 false
+func GetEnvBool(key string, def bool) (bool, bool) {
 	if value, ok := os.LookupEnv(key); ok {
 		bv, _ := strconv.ParseBool(value)
-		return bv
+		return bv, ok
 	}
-	return def
+	return def, false
 }
