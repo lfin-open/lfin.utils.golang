6b3931d4c607180b676611ce44af4f3a4e29e05e
Ted KIM <ted.kim@lfin.kr>
2023-03-07 10:53:47 +0900

    time.Duration, float 가져오기 추가

--- env/env.go
+++ env/env.go
@@ -3,6 +3,7 @@ package env
 import (
 	"os"
 	"strconv"
+	"time"
 )
 
 // SetEnv 환경변수 설정
@@ -16,8 +17,9 @@ func SetEnv(key, value string) error {
 // key: 환경변수명
 // def: 기본값
 // return:
-//  string: 환경변수값, 키가 없으면 def 값
-//  bool: 키가 있으면 true, 없으면 false
+//
+//	string: 환경변수값, 키가 없으면 def 값
+//	bool: 키가 있으면 true, 없으면 false
 func GetEnv(key, def string) (string, bool) {
 	if value, ok := os.LookupEnv(key); ok {
 		return value, ok
@@ -29,11 +31,15 @@ func GetEnv(key, def string) (string, bool) {
 // key: 환경변수명
 // def: 기본값
 // return:
+//
 //	int: 환경변수값, 키가 없으면 def 값
-//	bool: 키가 있으면 true, 없으면 false
+//	bool: 키가 있으면 true, 없거나 int 로 변환할 수 없으면 false
 func GetEnvInt(key string, def int) (int, bool) {
 	if value, ok := os.LookupEnv(key); ok {
-		iv, _ := strconv.Atoi(value)
+		iv, err := strconv.Atoi(value)
+		if err != nil {
+			return def, false
+		}
 		return iv, ok
 	}
 	return def, false
@@ -43,12 +49,52 @@ func GetEnvInt(key string, def int) (int, bool) {
 // key: 환경변수명
 // def: 기본값
 // return:
-//  int64: 환경변수값, 키가 없으면 def 값
-//  bool: 키가 있으면 true, 없으면 false
+//
+//	int64: 환경변수값, 키가 없으면 def 값
+//	bool: 키가 있으면 true, 없거나 int64 로 변환할 수 없으면 false
 func GetEnvInt64(key string, def int64) (int64, bool) {
 	if value, ok := os.LookupEnv(key); ok {
-		iv, _ := strconv.ParseInt(value, 10, 64)
+		iv, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return def, false
+		}
 		return iv, ok
+	}
+	return def, false
+}
+
+// GetEnvFloat32 환경변수 가져오기
+// key: 환경변수명
+// def: 기본값
+// return:
+//
+//	float32: 환경변수값, 키가 없으면 def 값
+//	bool: 키가 있으면 true, 없거나 float32 로 변환할 수 없으면 false
+func GetEnvFloat32(key string, def float32) (float32, bool) {
+	if value, ok := os.LookupEnv(key); ok {
+		fv, err := strconv.ParseFloat(value, 32)
+		if err != nil {
+			return def, false
+		}
+		return float32(fv), ok
+	}
+	return def, false
+}
+
+// GetEnvFloat64 환경변수 가져오기
+// key: 환경변수명
+// def: 기본값
+// return:
+//
+//	float64: 환경변수값, 키가 없으면 def 값
+//	bool: 키가 있으면 true, 없거나 float64 로 변환할 수 없으면 false
+func GetEnvFloat64(key string, def float64) (float64, bool) {
+	if value, ok := os.LookupEnv(key); ok {
+		fv, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return def, false
+		}
+		return fv, ok
 	}
 	return def, false
 }
@@ -57,12 +103,34 @@ func GetEnvInt64(key string, def int64) (int64, bool) {
 // key: 환경변수명
 // def: 기본값
 // return:
-//  bool: 환경변수값, 키가 없으면 def 값, 1,True,true-->true, 그외 false
-//  bool: 키가 있으면 true, 없으면 false
+//
+//	bool: 환경변수값, 키가 없으면 def 값, 1,True,true-->true, 그외 false
+//	bool: 키가 있으면 true, 없거나 bool 로 변환할 수 없으면 false
 func GetEnvBool(key string, def bool) (bool, bool) {
 	if value, ok := os.LookupEnv(key); ok {
-		bv, _ := strconv.ParseBool(value)
+		bv, err := strconv.ParseBool(value)
+		if err != nil {
+			return def, false
+		}
 		return bv, ok
+	}
+	return def, false
+}
+
+// GetEnvTimeDuration 환경변수 가져오기
+// key: 환경변수명
+// def: 기본값
+// return:
+//
+//	time.Duration: 환경변수값, 키가 없으면 def 값
+//	bool: 키가 있으면 true, 없거나 duration 으로 변환할 수 없으면 false
+func GetEnvTimeDuration(key string, def time.Duration) (time.Duration, bool) {
+	if value, ok := os.LookupEnv(key); ok {
+		duration, err := time.ParseDuration(value)
+		if err != nil {
+			return def, false
+		}
+		return duration, ok
 	}
 	return def, false
 }
--- env/env_test.go
+++ env/env_test.go
@@ -1,9 +1,11 @@
 package env
 
 import (
+	"fmt"
 	"os"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -60,15 +62,15 @@ func TestGetEnvInt(t *testing.T) {
 	args := []args{
 		{"TEST_I_1", "123456", 654321, 123456, true},
 		{"TEST_I_2", "-91", -19, -91, true},
-		{"TEST_I_3", "1911A", 1191, 0, true},
-		{"TEST_I_4", "2 9 3", 392, 0, true},
+		{"TEST_I_3", "1911A", 1191, 1191, false},
+		{"TEST_I_4", "2 9 3", 392, 392, false},
 	}
 
 	for _, arg := range args {
 		SetEnv(arg.key, arg.value)
 		value, exist := GetEnvInt(arg.key, int(reflect.ValueOf(arg.def).Int()))
-		if !exist {
-			t.Errorf("Not found env key [%s]", arg.key)
+		if !assert.Equal(t, arg.exist, exist) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.exist, exist)
 		}
 		if !assert.Equal(t, arg.expected, value) {
 			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.expected, value)
@@ -85,15 +87,15 @@ func TestGetEnvInt64(t *testing.T) {
 	args := []args{
 		{"TEST_I_1", "123456", int64(654321), int64(123456), true},
 		{"TEST_I_2", "-91", int64(-19), int64(-91), true},
-		{"TEST_I_3", "1911A", int64(1191), int64(0), true},
-		{"TEST_I_4", "2 9 3", int64(392), int64(0), true},
+		{"TEST_I_3", "1911A", int64(1191), int64(1191), false},
+		{"TEST_I_4", "2 9 3", int64(392), int64(392), false},
 	}
 
 	for _, arg := range args {
 		SetEnv(arg.key, arg.value)
 		value, exist := GetEnvInt64(arg.key, reflect.ValueOf(arg.def).Int())
-		if !exist {
-			t.Errorf("Not found env key [%s]", arg.key)
+		if !assert.Equal(t, arg.exist, exist) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.exist, exist)
 		}
 		if !assert.Equal(t, arg.expected, value) {
 			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.expected, value)
@@ -106,25 +108,76 @@ func TestGetEnvInt64(t *testing.T) {
 	assert.Equal(t, int64(99), v)
 }
 
+func TestGetEnvFloat32(t *testing.T) {
+	args := []args{
+		{"TEST_F32_1", "123456", float32(654321), float32(123456), true},
+		{"TEST_F32_2", "-91", float32(-19), float32(-91), true},
+		{"TEST_F32_3", "1911A", float32(1191), float32(1191), false},
+		{"TEST_F32_4", "2 9 3", float32(392), float32(392), false},
+	}
+
+	for _, arg := range args {
+		SetEnv(arg.key, arg.value)
+		value, exist := GetEnvFloat32(arg.key, float32(reflect.ValueOf(arg.def).Float()))
+		if !assert.Equal(t, arg.exist, exist) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.exist, exist)
+		}
+		if !assert.Equal(t, arg.expected, value) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.expected, value)
+		}
+	}
+
+	// default value if not found
+	v, exist := GetEnvFloat32("NOT_FOUND_I", float32(99))
+	assert.Equal(t, false, exist)
+	assert.Equal(t, float32(99), v)
+}
+
+func TestGetEnvFloat64(t *testing.T) {
+	args := []args{
+		{"TEST_F64_1", "123456", float64(654321), float64(123456), true},
+		{"TEST_F64_2", "-91", float64(-19), float64(-91), true},
+		{"TEST_F64_3", "1911A", float64(1191), float64(1191), false},
+		{"TEST_F64_4", "2 9 3", float64(392), float64(392), false},
+	}
+
+	for _, arg := range args {
+		SetEnv(arg.key, arg.value)
+		value, exist := GetEnvFloat64(arg.key, reflect.ValueOf(arg.def).Float())
+		if !assert.Equal(t, arg.exist, exist) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.exist, exist)
+		}
+		if !assert.Equal(t, arg.expected, value) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.expected, value)
+		}
+	}
+
+	// default value if not found
+	v, exist := GetEnvFloat64("NOT_FOUND_I", float64(99))
+	assert.Equal(t, false, exist)
+	assert.Equal(t, float64(99), v)
+}
+
 func TestGetEnvBool(t *testing.T) {
 	args := []args{
 		{"TEST_B_01_true", "true", false, true, true},
 		{"TEST_B_02_True", "True", false, true, true},
-		{"TEST_B_03_truE", "truE", false, false, true},
+		{"TEST_B_03_truE", "truE", false, false, false},
 		{"TEST_B_04_1", "1", false, true, true},
 		{"TEST_B_05_0", "0", true, false, true},
 		{"TEST_B_06_false", "false", true, false, true},
 		{"TEST_B_07_False", "False", true, false, true},
-		{"TEST_B_08_2", "2", true, false, true},
-		{"TEST_B_09_-1", "-1", true, false, true},
-		{"TEST_B_10_EMPTY_TRUE", "", true, false, true},
+		{"TEST_B_08_2", "2", false, false, false},
+		{"TEST_B_09_-1", "-1", false, false, false},
+		{"TEST_B_10_EMPTY_TRUE", "", false, false, false},
+		{"TEST_B_11_PARSE_ERROR", "ABC", false, false, false},
 	}
 
 	for _, arg := range args {
 		SetEnv(arg.key, arg.value)
 		value, exist := GetEnvBool(arg.key, reflect.ValueOf(arg.def).Bool())
-		if !exist {
-			t.Errorf("Not found env key [%s]", arg.key)
+		if !assert.Equal(t, arg.exist, exist) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.exist, exist)
 		}
 		if !assert.Equal(t, arg.expected, value) {
 			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.expected, value)
@@ -138,4 +191,32 @@ func TestGetEnvBool(t *testing.T) {
 	vt, exist := GetEnvBool("NOT_FOUND_BT", true)
 	assert.Equal(t, false, exist)
 	assert.Equal(t, true, vt)
+}
+
+func TestGetEnvTimeDuration(t *testing.T) {
+	args := []args{
+		{"TEST_T_01_10sec", "10s", "10s", "10s", true},
+		{"TEST_T_02_60m", "60m", "60m", "60m", true},
+		{"TEST_T_03_5ms", "5ms", "5ms", "5ms", true},
+		{"TEST_T_04_0sec", "0s", "0s", "0s", true},
+		{"TEST_T_05_ABC", "ABC", "9s", "9s", false},
+	}
+
+	for _, arg := range args {
+		SetEnv(arg.key, arg.value)
+		def, _ := time.ParseDuration(fmt.Sprintf("%v", reflect.ValueOf(arg.def).String()))
+		exp, _ := time.ParseDuration(fmt.Sprintf("%v", reflect.ValueOf(arg.expected).String()))
+		value, exist := GetEnvTimeDuration(arg.key, def)
+		if !assert.Equal(t, arg.exist, exist) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, arg.exist, exist)
+		}
+		if !assert.Equal(t, exp.Seconds(), value.Seconds()) {
+			t.Errorf("key:%s, value:%s, expected:%v, actual:%v", arg.key, arg.value, exp.Seconds(), value.Seconds())
+		}
+
+		v, exist := GetEnvTimeDuration("NOT_FOUND_BF", time.Duration(99))
+		assert.Equal(t, false, exist)
+		assert.Equal(t, time.Duration(99).Seconds(), v.Seconds())
+
+	}
 }
